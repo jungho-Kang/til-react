5c95b64c6b612968db681b8a5c0cd51642c0b0a7
jungho-Kang <rkdwjdgh08@gmail.com>
2024-11-25 12:13:57 +0900

    [jsx] 기초

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import IndexPage from "./pages/IndexPage";
 
-createRoot(document.getElementById('root')).render(
+// Page라는 이름이 붙어있다.
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <IndexPage></IndexPage>
   </StrictMode>,
-)
+);
